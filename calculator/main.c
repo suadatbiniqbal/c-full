a9445949265b49ec049fef47e05e819483ab3cf2
Vetra <suadatbiniqbal@outlook.com>
2026-03-26 23:25:26 +0530

    calculator in c

---
+++ calculator/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double num1;
+    double num2;
+    printf("Enter First Number;");
+    scanf("%lf", &num1);
+    printf("Enter Second  Number;");
+    scanf("%lf", &num2);
+
+    printf("Answer is %f", num1 + num2);
+
+
+    return 0;
+}
