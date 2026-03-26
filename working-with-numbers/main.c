d53bafc7aa973ef4b9daa8195eccde1a779d3695
Vetra <suadatbiniqbal@outlook.com>
2026-03-26 22:51:28 +0530

    playingwithnumbers

---
+++ working-with-numbers/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+
+
+    printf("%f\n", pow(2, 3) );
+    printf("%f\n", sqrt(45) );
+     printf("%f", floor(4.898) );
+    printf("%f", ceil(4.898) );
+
+    return 0;
+
+}
