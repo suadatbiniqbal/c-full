fd570e73bf63ff0dded45fe4ec7b3725112feb7c
Vetra <suadatbiniqbal@outlook.com>
2026-03-26 22:13:23 +0530

    variabels

---
+++ hello/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char charcterName[] = "Tom";
+    int charcterAge = 35;
+
+
+
+    printf("Hey  %s\n", charcterName);
+
+    printf("your dad is %d", charcterAge);
+
+    charcterAge = 45;
+
+    printf(" and your age is %d and you love amber/n ", charcterAge);
+    return 0;
+}
