42c011da40c96954fc49942a20c2ea141b27d34c
Vetra <suadatbiniqbal@outlook.com>
2026-03-26 22:27:50 +0530

    datatypes

---
+++ datatype/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40;
+    double gpa = 3.7;
+    char grade ='A';
+    char phrse [] = "Suadat is a good boy ";
+    printf("Your grade is %f", gpa);
+    printf("and your age is %d", age);
+
+    return 0;
+}
