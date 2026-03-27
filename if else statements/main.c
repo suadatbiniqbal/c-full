7aa6cff028d4c2fde29469be0329adcc87d2fcad
Vetra <suadatbiniqbal@outlook.com>
2026-03-27 22:11:23 +0530

    if elseand end of the course imma learn ts again in college now lol

---
+++ if else statements/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max (int num1, int num2) {
+int result;
+if (num1>num2) {
+    reult = num1;
+}
+else {
+    result = num2
+}
+}
+int main()
+{
+    printf("%d\n",max,4,0);
+    return 0;
+}
