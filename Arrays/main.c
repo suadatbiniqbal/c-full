372197bde06c78c728e5e18ac8a32edb0f1d96f4
Vetra <suadatbiniqbal@outlook.com>
2026-03-27 12:13:12 +0530

    arrays

---
+++ Arrays/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*Arrays*/
+
+    int luckyNumbers[] = {2,4,8,16,32};
+    luckyNumbers [1] = 200;
+    printf("%d \n", luckyNumbers [1]);
+    /*Assign Some Value*/
+
+    int newLuckyNum [10];
+    newLuckyNum [3] = 90;
+    printf("%d", luckyNumbers [3]);
+
+
+    return 0;
+}
